23e08b68fc50facde7b9a1fec049dadb987eccd4
dczinil.tython <dczinil@gmail.com>
2023-03-01 14:12:21 -0600

    delete the create of virtuals machines for the services of kubernetes individual, it was concluded generating a single server for all the services with H/A

--- ansible/script/hosts.sh
+++ ansible/script/hosts.sh
@@ -3,48 +3,36 @@
 IP_NW="192.168.56."
 #
 
+echo "#-***-#_#-***-#"
 i=10
 while (($i<20))
 do
-  echo export K8S-ETCD-$i=$IP_NW$i 
+  echo "export K8SETCD$i="$IP_NW$i""
   ((i++))
 done
-
 
 i=20
 while (($i<30))
 do
-  echo export K8S-KAPI-$i=$IP_NW$i 
-  ((i++))
-done
-
-i=30
-while (($i<40))
-do
-  echo export K8S-KCM-$i=$IP_NW$i 
-  ((i++))
-done
-
-i=40
-while (($i<50))
-do
-  echo export K8S-KSC-$i=$IP_NW$i 
+  echo "export K8SKACS$i="$IP_NW$i""
   ((i++))
 done
 
 i=50
 while (($i<60))
 do
-  echo export K8S-WORKER-$i=$IP_NW$i 
+  echo export K8SWORKER$i="$IP_NW$i"
   ((i++))
 done
 
 i=60
 while (($i<70))
 do
-  echo export K8S-LB-$i=$IP_NW$i 
+  echo export K8SLB$i="$IP_NW$i"
   ((i++))
 done
 
-export SERVICE_CIDR=10.96.0.0/24
-export API_SERVICE=$(echo $SERVICE_CIDR | awk 'BEGIN {FS="."} ; { printf("%s.%s.%s.1", $1, $2, $3) }')
+echo export SERVICE_CIDR="10.96.0.0/24"
+echo export API_SERVICE="$(echo $SERVICE_CIDR | awk 'BEGIN {FS="."} ; { printf("%s.%s.%s.1", $1, $2, $3) }')"
+echo "#-***-#_#-***-#"
+exit 0
